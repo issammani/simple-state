ca3ac10acc85c64a89bcb7e18260d5397c939863
Issam Mani <issamouu69@gmail.com>
2022-08-23 20:31:38 +0200

    genesis: let there be code

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2016,
+    sourceType: 'module',
+  },
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  rules: {},
+};
