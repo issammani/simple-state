15b5a9d4b9f0eee46f5a1ea669c0e211e38c2aba
Issam Mani <issamouu69@gmail.com>
2022-08-23 20:56:46 +0200

    feat: add basic observable with getter and setter

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
-console.log("Ohai World !!!");
+type GetterFn<T> = () => T;
+type SetterFn<T> = (value: T) => void;
+type Observable = <T>(value: T) => [GetterFn<T>, SetterFn<T>];
+
+export const observable: Observable = <T>(value: T) => {
+  let _value = value;
+  const getter = () => _value;
+  const setter = (newValue: T) => {
+    _value = newValue;
+  };
+  return [getter, setter];
+};
+
+// TODO: add these as tests later
+const [primitive, setPrimitive] = observable<string>('John');
+const [object, setObject] = observable<{ name: string; age: number }>({
+  name: 'John',
+  age: 30,
+});
+const [array, setArray] = observable<number[]>([10, 20, 30]);
+
+console.log(primitive());
+console.log(object());
+console.log(array());
+
+setPrimitive('Jane');
+setObject({ name: 'Jane', age: 31 });
+setArray([40, 50, 60]);
+
+console.log(primitive());
+console.log(object());
+console.log(array());
