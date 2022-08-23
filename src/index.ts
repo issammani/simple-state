cf82336095708c5c5243e8125acf124a69c92b23
Issam Mani <issamouu69@gmail.com>
2022-08-23 21:22:41 +0200

    feat: add subsribers and side effects

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,58 @@
 type GetterFn<T> = () => T;
 type SetterFn<T> = (value: T) => void;
 type Observable = <T>(value: T) => [GetterFn<T>, SetterFn<T>];
+type EffectCallback = () => any;
+type Effect = (effectCallback: EffectCallback) => void;
+
+// This global variable is used to get the currently running effect.
+// We need this to be able to dynamically track dependencies without needing
+// to pass a dependency array to every effect.
+let currentEffectFn: EffectCallback | null = null;
 
 export const observable: Observable = <T>(value: T) => {
+  // Since we want to notify observers of eventual changes, we need to keep track
+  // of the subscribers. We use a set to avoid duplicates.
+  const subscribers: Set<(value: T) => void> = new Set();
+
+  // Keep track of the current value internally
   let _value = value;
-  const getter = () => _value;
+
+  const getter = () => {
+    // Check if we are calling the getter from within an effect.
+    // If so, we need to track the dependency.
+    if (currentEffectFn) {
+      subscribers.add(currentEffectFn);
+    }
+    return _value;
+  };
   const setter = (newValue: T) => {
     _value = newValue;
+    // Notify all subscribers of the change
+    subscribers.forEach((callback) => callback(newValue));
   };
+
   return [getter, setter];
+};
+
+export const effect: Effect = (effectCallback) => {
+  // We set the current effect to be the effect we are currently running
+  // Then we call the effect function. This will in turn call the getter.
+  // This way we can keep track of the effectCallback as a dependency
+  // of the observable values it uses.
+  currentEffectFn = effectCallback;
+  effectCallback();
+  currentEffectFn = null; // Reset the current effect callback function.
 };
 
 // TODO: add these as tests later
 const [primitive, setPrimitive] = observable<string>('John');
-const [object, setObject] = observable<{ name: string; age: number }>({
-  name: 'John',
-  age: 30,
+console.log(primitive()); // John
+
+// This effect will run at least once.
+effect(() => {
+  // This console.log should be called on each state change !
+  console.log('From effect --- ', primitive());
 });
-const [array, setArray] = observable<number[]>([10, 20, 30]);
 
-console.log(primitive());
-console.log(object());
-console.log(array());
-
-setPrimitive('Jane');
-setObject({ name: 'Jane', age: 31 });
-setArray([40, 50, 60]);
-
-console.log(primitive());
-console.log(object());
-console.log(array());
+setPrimitive('Jane'); // Should trigger the effect
+setPrimitive('Bob'); // Should not trigger the effect
